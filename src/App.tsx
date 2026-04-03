0425f42448cd4e10621c06b373bd04e82fe1e68a
Densyakun <densyakun1128@gmail.com>
2026-04-03 14:21:45 +0900

    ギズモ操作中にカメラを操作しないようにする

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function Scene() {
         )
       })}
       <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
-      <OrbitControls />
+      <OrbitControls makeDefault />
     </>
   )
 }
